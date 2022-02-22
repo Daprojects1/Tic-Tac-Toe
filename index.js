ce3a4751791419b4f30bf7c7b9fa3c7ab1a11ca2
Daprojects1 <davidrtm45@gmail.com>
2022-02-22 14:40:37 +0000

    incomplete, still working on main logic

--- index.js
+++ index.js
@@ -12,18 +12,20 @@ let _currentPlaying = new WeakMap();
 let _boardfunc = new WeakMap();
 let _arrValsX = new WeakMap();
 let _arrValsO = new WeakMap();
+let _bgIndex1 = new WeakMap();
+let _bgIndex2 = new WeakMap();
 class TicTacToe {
     constructor() {
         _counter.set(this, 0);
         _checks.set(this, false);
         // function that grabs names;
         _vals.set(this, () => {
-            let p3 = prompt("First Player Name ?")
-            let p2 = prompt("Second Player Name ? ")
-            p1Name.innerText = p3;
-            p2Name.innerText = p2;
-            _player1.set(this, p3);
-            _player2.set(this, p2);
+            // let p3 = prompt("First Player Name ?")
+            // let p2 = prompt("Second Player Name ? ")
+            // p1Name.innerText = p3;
+            // p2Name.innerText = p2;
+            // _player1.set(this, p3);
+            // _player2.set(this, p2);
         })
         _arrValsX.set(this, []);
         _arrValsO.set(this, []);
@@ -42,9 +44,11 @@ class TicTacToe {
         _boardfunc.set(this, () => {
             let arrX = _arrValsX.get(this);
             let arrO = _arrValsO.get(this);
+            let checkforDone = 0;
             for (let i = 0; i < Board.children.length; i++) {
                 Board.children[i].setAttribute("data-id", i);
                 let changeColor = () => {
+                    checkforDone++;
                     let XorO = _currentPlaying.get(this)();
                     Board.children[i].innerText = XorO;
                     if (Board.children[i].innerText === "x") {
@@ -58,12 +62,17 @@ class TicTacToe {
                         arrO.push(attrX);
                         console.log("ar2", arrO);
                     };
+                    if (checkforDone === 9) {
+                        this.drawCheck();
+                    }
+                    this.winCheck();
                     Board.children[i].removeEventListener("click", changeColor)
                 }
                 Board.children[i].addEventListener("click", changeColor)
             }
         })
     }
+    // allows us to get the names and store it 
     Names() {
         let counter = _counter.get(this)
         if (!counter) {
@@ -71,6 +80,7 @@ class TicTacToe {
         }
         this.BoardEv();
     }
+    // the main board function.
     BoardEv() {
         let counter = _counter.get(this)
         if (!counter) {
@@ -78,8 +88,67 @@ class TicTacToe {
             _boardfunc.get(this)();
         }
     }
+    // still working on this function.
+    winCheck() {
+        let winningVals = [
+            ["210", "012", "120", "102", "201", "021"],
+            ["345", "354", "435", "453", "543", "534"],
+            ["678", "687", "768", "786", "867", "876"],
+            ["036", "063", "306", "360", "603", "630"],
+            ["147", "174", "417", "471", "741", "714"],
+            ["258", "285", "528", "582", "825", "852"],
+            ["048", "084", "480", "408", "840", "804"],
+            ["264", "246", "426", "462", "624", "642"]
+        ];
+        let arrX = _arrValsX.get(this).join("");
+        let arrO = _arrValsO.get(this).join("");
+        let player1Check = winningVals.flatMap((item) => {
+            let bool;
+            item.map((vals) => {
+                if (arrX.includes(vals)) {
+                    _bgIndex1.set(this, vals)
+                    bool = true;
+                }
+            })
+            return bool;
+        })
+        let player2Check = winningVals.flatMap((item) => {
+            let bool;
+            item.map((vals) => {
+                if (arrO.includes(vals)) {
+                    _bgIndex2.set(this, vals)
+                    bool = true;
+                }
+            })
+            return bool
+        })
+
+        if (player1Check.includes(true)) {
+            let cBgColor = _bgIndex1.get(this);
+            Board.children[cBgColor[0]].classList.add("bg1")
+            Board.children[cBgColor[1]].classList.add("bg1")
+            Board.children[cBgColor[2]].classList.add("bg1")
+            alert(`${_player1.get(this)} has won !`);
+        } else if (player2Check.includes(true)) {
+            let cBgColor2 = _bgIndex2.get(this);
+            alert(`${_player2.get(this)} has won!`);
+        }
+    }
+    drawCheck() {
+        if (!_bgIndex1.get(this) && !_bgIndex2.get(this)) {
+            alert("This is a draw, play again!")
+            location.reload();
+        }
+    }
+    Reset() {
+        let btn = document.querySelector(".mainbtn")
+        btn.addEventListener("click", () => {
+            location.reload();
+        })
+    }
     static Run() {
-        return new TicTacToe().Names();
+        new TicTacToe().Names();
+        return new TicTacToe().Reset();
     }
 }
 
