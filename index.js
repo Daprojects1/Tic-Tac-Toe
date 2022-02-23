4c02666b83c1970b8f458fa1cd225fd4c9b930c8
Daprojects1 <davidrtm45@gmail.com>
2022-02-23 12:56:38 +0000

    Main vals

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         // still working on this function.
         winCheck() {
+            // all the winning values;
             let winningVals = [["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"], ["0", "3", "6"], ["1", "4", "7"], ["2", "5", "8"], ["0", "4", "8"], ["2", "6", "4"]];
             let arrX = _arrValsX.get(this);
             let arrO = _arrValsO.get(this);
